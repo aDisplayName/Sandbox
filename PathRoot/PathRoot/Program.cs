c82b09a201102889f98fa916bc6e077470be40c4
aDisplayName <mattali@gmail.com>
2019-11-10 11:57:15 +0800

    PathRoot
    Add code to test path root and qualification status

---
+++ PathRoot/PathRoot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace PathRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TryPath(@"C:/a/b.txt");
+            TryPath(@"C:a/b.txt");
+            TryPath(@"/a/b.txt");
+            TryPath(@"a/b.txt");
+        }
+
+        private static void TryPath(string s)
+        {
+            Console.WriteLine($"path: {s} IsRooted {Path.IsPathRooted(s)}; IsFullyQualified {Path.IsPathFullyQualified(s)}");
+        }
+    }
+}
